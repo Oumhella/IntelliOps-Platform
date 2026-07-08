23ad1e44b3e99ef6dec44f8b42c26abaad3529f2
Oumhella <aoumhella98@gmail.com>
2026-07-08 13:47:56 +0100

    initialize microservices with Docker, Maven wrapper, and Spring Boot configurations

---
+++ lead_service/src/test/java/org/example/lead_service/LeadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lead_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
